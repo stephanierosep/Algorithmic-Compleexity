ad0fafb4b6e2a64b50565934670c4007d9495b22
Stephanie Parker <stephanieparker@MacBook-Pro-3.local>
2018-12-17 16:20:58 +0000

    amended code to include average and median

--- timing_code.rb
+++ timing_code.rb
@@ -2,10 +2,13 @@ counter = 1
 rangemin = 0
 RANGEMAX = 100000
 while rangemin != RANGEMAX
- rangemin = rangemin + 5000
- counter = 1
- puts "next array #{rangemin}"
- while counter <= 5
+  rangemin = rangemin + 5000
+  results = []
+  counter = 1
+  # puts "next array #{rangemin}"
+
+
+  while counter <= 15
     start = Time.now
     array = []
     range = (1..rangemin)
@@ -13,9 +16,15 @@ while rangemin != RANGEMAX
       array.push(n)
     end
 
- finish = Time.now
- result = finish - start
- counter += 1
- puts result
+    finish = Time.now
+    result = finish - start
+    counter += 1
+    results << result
   end
+
+  avg = results.sum / results.length
+  # median
+  median = results.sort[6]
+  puts "#{rangemin}\t#{avg}\t#{median}"
+
 end
