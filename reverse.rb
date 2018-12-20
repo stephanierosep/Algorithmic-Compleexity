858803bdb7f72143d19829060c969f1f9fb44393
Stephanie Parker <stephanieparker@MacBook-Pro-3.local>
2018-12-20 15:33:31 +0000

    algorithms for specific functions

--- reverse.rb
+++ reverse.rb
@@ -1,13 +1,3 @@
-def reverse_order(array)
-  reversed = []
-  until array.length == 0
-    number = array.pop
-    reversed << number
-  end
-  return reversed
-end
-
-
 counter = 1
 rangemin = 0
 RANGEMAX = 100000
@@ -15,12 +5,8 @@ while rangemin != RANGEMAX
   rangemin = rangemin + 5000
   results = []
   counter = 1
-  # puts "next array #{rangemin}"
-
 
   while counter <= 15
-
-    # setup
     array = []
     range = (1..rangemin)
     range.each do |n|
@@ -28,20 +14,16 @@ while rangemin != RANGEMAX
     end
 
     start = Time.now
-    #  code to test
-    reverse_order(array)
-
+    array.reverse
     finish = Time.now
-
-    # storing results
     result = finish - start
     counter += 1
     results << result
   end
 
-  avg = results.sum / results.length
+  avg = results.sum.to_f / results.length
   # median
-  median = results.sort[6]
+  median = results.sort[8]
   puts "#{rangemin}\t#{avg}\t#{median}"
 
 end
